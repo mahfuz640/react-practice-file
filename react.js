8a52ff2c3d249a8b66cd4bb8bc713b82e3ff142f
rabbi640 <mdmahfuz640@gmail.com>
2023-03-19 22:26:26 +0600

    first commit

--- react.js
+++ react.js
@@ -4,3 +4,8 @@ export default function Message (){
         <h1 style={{backgroundColor:"ffa45b",textAline:"center"}}>Welcome to React</h1>
     )
 }
+
+// npm ---- holo permently create react app k niye neya
+// npx ---- ar mane holo jokhon dorkar hobe tokhon niye nibo
+// npx create-react-app ------ . dile kono folder add hobe na  .  na dile folder name dile oy folder name show korbe
+// npm start--- dile react start hobe
